328154b473ddf8ab2524a63f0b1b9f8da0281a24
Mircea Nistor <mirceanis@gmail.com>
2019-03-18 15:52:02 +0200

    clean imports

--- android/src/main/java/com/reactlibrary/RNUportSignerPackage.kt
+++ android/src/main/java/com/reactlibrary/RNUportSignerPackage.kt
@@ -1,12 +1,12 @@
+@file:Suppress("unused")
+
 package com.reactlibrary
 
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
-
-import java.util.ArrayList
-import java.util.Collections
+import java.util.*
 
 /**
  * package class as required by react native integration.
