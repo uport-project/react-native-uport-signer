c13aba9ff5a2557129881f7418582d9146213db9
Mircea Nistor <mirceanis@gmail.com>
2019-09-17 09:27:48 +0200

    fix: add build scripts and linting options
    fixes #36

--- index.ts
+++ index.ts
@@ -8,20 +8,20 @@ const RNUportHDSigner: any = NativeModules.RNUportHDSignerModule;
 const getSignerForHDPath = (
   seedAlias: string,
   derivationPath: string = RNUportHDSigner.UPORT_ROOT_DERIVATION_PATH,
-  userPrompt = "" ): object => {
+  userPrompt = ''): object => {
 
   return async (data: string) => {
     const { v, r, s } = await RNUportHDSigner.signJwt(seedAlias,
       derivationPath,
       Buffer.from(data).toString('base64'),
       userPrompt);
-    
+
     return {
-      recoveryParam: ((v == 0 || v == 1) ? v : v - 27),
+      recoveryParam: ((v === 0 || v === 1) ? v : v - 27),
       r: Buffer.from(r, 'base64').toString('hex'),
-      s: Buffer.from(s, 'base64').toString('hex'),
-    }
-  }
-}
+      s: Buffer.from(s, 'base64').toString('hex')
+    };
+  };
+};
 
 export { RNUportSigner, RNUportHDSigner, getSignerForHDPath };
